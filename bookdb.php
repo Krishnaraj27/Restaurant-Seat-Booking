4f76df3e7ab628b2154319641ae8de0ebcab660d
Krishnarajsinh Gohil <gkrishnaraj2002@gmail.com>
2023-06-16 12:51:29 +0530

    Update bookdb.php
    Updated coupon db and excluded db connection in another file

--- bookdb.php
+++ bookdb.php
@@ -1,37 +1,30 @@
 <?php
 
+    include('./db.php');
+
    // Get the form data
     $bookingdate = $_POST['bookingdate'];
     $daytype = $_POST['daytype'];
     $session = $_POST['session'];
     $email = $_POST['email'];
-    // $mobile = $_POST['mobile'];
     $seats = $_POST['seatsid'];
     $price = $_POST['totalPrice'];
     $count = $_POST['count'];
     $name = $_POST['name'];
     $mobile = $_POST['number'];
+    $coupon = $_POST['cpnSelect'];
+    $discount = $_POST['discount'];
+    $netTotal = $_POST['netTotal'];
 
     // $dt = $bookingdate->format('Y-m-d H:i:s');
     // $date = substr($dt,0,10);
-    // Connect to the database
-    $servername = "localhost"; 
-    $username = "root"; 
-    $password = ""; 
-    $dbname = "booking"; 
-    
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
-    
-    // Check connection
-    if (!$conn) {
-      die("Connection failed: " . mysqli_connect_error());
-    }
     
     
     // Insert the data into the bookings table
-    $sql = "INSERT INTO `booked` (`booking_id` , `date` , `email` , `session` , `name`, `mobile`, `count` , `price` ,`seats_id`) VALUES  (NULL,'$bookingdate', '$email', '$session','$name','$mobile', '$count','$price','$seats')";
+    $sql = "INSERT INTO `booked` (`booking_id` , `date` , `email` , `session` , `name`, `mobile`, `count` , `price` ,`seats_id`,`coupon_code`,`discount_amount`,`total`) VALUES  (NULL,'$bookingdate', '$email', '$session','$name','$mobile', '$count','$price','$seats','$coupon','$discount','$netTotal')";
     
     if (mysqli_query($conn, $sql)) {
+
       echo "<script>
             alert('Booking created successfully !!');
             window.location.href='./index.php';
